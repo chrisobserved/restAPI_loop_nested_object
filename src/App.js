9afa13d2dac796eb0b7b65ec7f5ec0fe53ba9344
Chris Bryant <chrisbryant.dev@gmail.com>
2019-04-10 01:59:17 -0400

    Syntax updates for readability, included normalize.css, code cleanup, refactored sorting to more modern ES6 + better readability, added a title header to the page, and updates to fade animation to be way more smooth

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+
+import 'normalize.css';
 import './App.css';
 
 class App extends Component {
@@ -7,45 +9,42 @@ class App extends Component {
 
     this.state = {
       members: []
-    };
+    }
   }
 
   componentDidMount() {
     fetch('https://h93rvy36y7.execute-api.us-east-1.amazonaws.com/teams')
       .then(response => response.json())
       .then(data => {
-        const va = data.filter(x => { return x.state === 'VA'; });
-        const members = va.map(x => { return x.members; })
+        const va = data.filter(x => x.state === 'VA' );
+        const members = va.map(x => x.members )
           .flat()
-          .filter(x => { return x.role === 'Technical Lead' || x.role === 'Software Engineer' })
-          .sort((p1, p2) => {
-            if (p1.lastName < p2.lastName) return -1;
-            if (p1.lastName > p2.lastName) return 1;
-            if (p1.firstName < p2.firstName) return -1;
-            if (p1.firstName > p2.firstName) return 1;
-            return 0;
-          })
-          .map(x => {
+          .filter(x => x.role === 'Technical Lead' || x.role === 'Software Engineer')
+          .sort((p1, p2) =>
+            (p1.lastName < p2.lastName) ? -1 :
+            (p1.lastName > p2.lastName) ? 1 :
+            (p1.firstName < p2.firstName) ? -1 :
+            (p1.firstName > p2.firstName) ? 1 : 0
+          )
+          .map(x => { 
             x.fullName = x.firstName + ' ' + x.lastName;
-            return x; 
+            return x
           });
-          
-        this.setState({members});
-      });
+        this.setState({ members });
+      })
   }
 
   render() {
     const { members } = this.state;
     return (
       <div className="App">
-      {
-        members.length > 0 &&
-        members.map(x => {
-          return (
+        <h2 className="membersHeader">VA Team Members</h2>
+        { 
+          members.length > 0 &&
+          members.map(x => 
             <div key={x.fullName} className="membersList">{x.fullName}</div>
           )
-        })
-      }
+        }
       </div>
     );
   }
